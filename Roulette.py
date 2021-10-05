df072bafc5fe20d82f5e3a884ff9a986d3feb102
DriftyX <85983500+DriftyX@users.noreply.github.com>
2021-10-05 11:20:40 +0400

    Add files via upload

--- Roulette.py
+++ Roulette.py
@@ -6,15 +6,52 @@ from tkinter import *
 # INTRODUCTION
 
 ## Player Profile Creation
+lst = []
+adjlist = [
+'mighty', 
+'fierce', 
+'incredible', 
+'ferocious', 
+'beautiful', 
+'charming', 
+'strong', 
+'marvellous', 
+'phenomenal', 
+'remarkable', 
+'spectacular', 
+'prodigious', 
+'dazzling', 
+'amazing', 
+'formidable',
+'astonishing']
+
+def adj_fn():
+    adj = adjlist[randint(0,len(adjlist))]
+    adjlist.pop(adjlist.index(adj))
+    return adj
+
+
 class Player:
     def __init__(self, name, cash=15000):
         self.name = name
         self.cash = float(cash)
 
-lst = []
-def input_fn():
-    players = int(input('Enter the number of players: '))
-    for i in range(1,players+1):
-        pname = Player(input(f'Enter Player {i}s name: '))
-        lst.append(pname)
+    def input_fn():
+        players = int(input('Enter the number of players: '))
+        for i in range(1,players+1):
+            pname = Player(input(f'Enter Player {i}s name: '))
+            lst.append(pname)
 
+    @classmethod
+    def print_fn(cls):
+        tempnamelist = []
+        for i in lst:
+            tempnamelist.append(i.name)
+        print(tempnamelist)
+            
+        name_list = (", ".join(zip(tempnamelist, adjlist)[:-1]))
+
+        # print(f'the ultimate game between {adj_fn()} {name_list} and {tempnamelist[-1]}!')
+
+p1 = Player('sa')
+p1.print_fn()
