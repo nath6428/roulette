583cd8abd5dbf062291e3bfff246de91bc9d01cc
Saif Nasir <68247296+SaifNasir196@users.noreply.github.com>
2021-10-06 16:13:02 +0400

    half done

--- Roulette.py
+++ Roulette.py
@@ -1,57 +1,129 @@
 from math import *
+# from os import stat
 from random import *
 from time import *
 from tkinter import *
 
 # INTRODUCTION
 
-## Player Profile Creation
-lst = []
-adjlist = [
-'mighty', 
-'fierce', 
-'incredible', 
-'ferocious', 
-'beautiful', 
-'charming', 
-'strong', 
-'marvellous', 
-'phenomenal', 
-'remarkable', 
-'spectacular', 
-'prodigious', 
-'dazzling', 
-'amazing', 
-'formidable',
-'astonishing']
+## Global vars
 
-def adj_fn():
-    adj = adjlist[randint(0,len(adjlist))]
-    adjlist.pop(adjlist.index(adj))
-    return adj
-
+adjlist = ['mighty', 'fierce', 'incredible', 'ferocious', 'beautiful', 'charming', 
+            'strong', 'marvellous', 'phenomenal', 'remarkable', 'spectacular', 
+            'prodigious', 'dazzling', 'amazing', 'formidable','astonishing']
+obj_list = []
+option_dic = {
+    'odd_even' : '1:1',
+    'firsthalf_secondhalf' : '1:1',
+    'primeNumber': '4:1',
+    'rangeOf10' : '10.1',
+    'fiveNumbers' : '20:1',
+    'fourNumbers' : '25:1',
+    'threeNumbers' : '33:1',
+    'twoNumbers' : '50:1',
+    'straight' : '100:1'
+}
 
 class Player:
+    name_list = []
+
     def __init__(self, name, cash=15000):
         self.name = name
         self.cash = float(cash)
-
-    def input_fn():
-        players = int(input('Enter the number of players: '))
-        for i in range(1,players+1):
-            pname = Player(input(f'Enter Player {i}s name: '))
-            lst.append(pname)
-
+        Player.Names(self)
+    
     @classmethod
-    def print_fn(cls):
-        tempnamelist = []
-        for i in lst:
-            tempnamelist.append(i.name)
-        print(tempnamelist)
-            
-        name_list = (", ".join(zip(tempnamelist, adjlist)[:-1]))
+    def Names(cls,self):
+        cls.name_list.append(self.name)  
+    
+    @classmethod
+    def print_names(cls):
+        print(cls.name_list)
+    
+    @classmethod
+    def intro_fn(cls):
+        string = 'The ultimate game between'
+        shuffle(adjlist)
+        for name, adj in zip(cls.name_list[:-1], adjlist):
+            string = string + f' the {adj} {name},'
+        string = string + f' and the {adjlist[-1]} {Player.name_list[-1]}!'
+        print(string+'\n')
 
-        # print(f'the ultimate game between {adj_fn()} {name_list} and {tempnamelist[-1]}!')
+    def __add__(self,other):
+        return self.cash + other.cash
 
-p1 = Player('sa')
-p1.print_fn()
+
+class Round:
+    def __init__(self, obj):
+        self.obj = obj
+
+    def initiate(cls):
+        global initiate
+        print('Ladies and gentleman!')
+        # sleep(1)
+        print('Welcome to the greatest roulette game of the century!')
+        # sleep(1.5)
+        Player.intro_fn()
+        # sleep(3)
+        initiate = 'y' #input('shall we begin? (y/n)')
+        
+    def print_prob(self):
+        n = 1
+        for i,j in zip(list(option_dic.keys()), list(option_dic.values())):
+            print(n,i,j)
+            n +=1
+        print('\n')
+    
+    def rules():
+
+        pass
+
+    @staticmethod
+    def rollDice():
+        return randint(0,32)
+    
+
+
+    def round(self):
+        
+        print('Choose a bet mf:')
+        for i in range(len(obj_list)):
+            choice = int(input(f'{self.obj[i].name}, enter bet number (1-9): '))
+
+            while choice not in range(1,10):
+                choice = int(input(f'{self.obj[i].name}, stop fkn around nigga, enter bet number again (1-9): '))
+        dice = Round.rollDice()
+
+
+        
+        
+
+def input_fn():
+    players = 3 #int(input('Enter the number of players: '))
+    return players
+
+def main():
+    players = input_fn()
+
+    for i in range(1,players+1):
+        player = Player('name'+str(i)) #input(f"Enter Player{i}'s gamertags: ")
+        obj_list.append(player)
+
+
+    Round.initiate(Round)
+    if initiate == 'n':
+        print('fairwell brothers and sisters')
+    else:
+        game = Round(obj_list) # create game instance
+
+        game.print_prob() # print probabilities
+        game.round()
+
+
+
+main()
+
+
+
+
+
