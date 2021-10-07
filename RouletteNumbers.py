d7fee56177786375f5018ad0781f6c593b102e3a
nath6428 <nathancrasto6@gmail.com>
2021-10-07 22:27:21 +0400

    not done yet, gonnaa add to main code

--- RouletteNumbers.py
+++ RouletteNumbers.py
@@ -1,15 +1,37 @@
 import random as r
+from typing import Dict
 import sympy as sym
+import tkinter as tk
 
+
+cash = 1
 
 numlist = [x for x in range(0,100)]
-print(numlist)
 
-number = numlist[r.randint(0,100)]
+
+number = 7
+firstHalfList = [x for x in range(0,50)]
 Evenlist = [x for x in range(0,100,2)]
-print(Evenlist)
 Oddlist = [item for item in numlist if item not in Evenlist]
-print(Oddlist) 
 Primelist = [x for x in sym.primerange(0,100)]
-print(Primelist)
+# rangeof10 = int(input('rangeend'))
+# if (rangeof10 < 10 or rangeof10 > 100):
+#         print('retry')
+# else:
+#     rangeof10List = [x for x in range(rangeof10-10, rangeof10)]
+#     print(rangeof10List)
+
+numlist_input = (input("1-5 Numbers").split(','))
+numlist_input = list(map(lambda x:int(x), numlist_input))
+
+oddsDict = {1: 100,
+        2: 50,
+        3: 33,
+        4: 25,
+        5: 20}
+if number in numlist_input:
+    cash = cash*(oddsDict[len(numlist_input)])
+
+print(number, cash)
+
 
