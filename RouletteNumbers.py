d76e987d0026f710df35d7f4b8cf7109f4ba9af4
Saif Nasir <68247296+SaifNasir196@users.noreply.github.com>
2021-10-10 19:03:50 +0400

    done

--- RouletteNumbers.py
+++ RouletteNumbers.py
@@ -1,55 +1,55 @@
 import random as r
-from typing import Dict
 import sympy as sym
 import tkinter as tk
 
-cash = 1
-
-
 numlist = [x for x in range(0,100)]
-number = r.randint(0,99)
-print(number)
 
 class bets():
     def __init__(self, cash):
         self.cash = cash
- 
-        
+   
+    @staticmethod
+    def rollWheel():
+        return r.randint(0,32)
+
     def firstHalf(self, HalfSelection):
-        firstHalfList = [x for x in range(0,50)]
+        # first = 1 | second = 2
         if HalfSelection == 1:
-            if number in firstHalfList:
-                self.cash = self.cash*2
+            if number <=49:
+                cash = self.cash*2
         else:
-            if number not in firstHalfList:
-                self.cash = self.cash*2
+            if number >49:
+                cash = self.cash*2
+        return cash
         
     def evenodd(self, EvenOddSelection):
         # Even = 1 | Odd = 2
-        Evenlist = [item for item in numlist if item%2==0]
         if EvenOddSelection == 1:
-            if number in Evenlist:
+            if number%2 == 0:
                 self.cash = self.cash*2
         else:
-            if number not in Evenlist:
+            if number%2 != 0:
                 self.cash = self.cash*2
+        return self.cash
             
 
     def prime(self):
         Primelist = [x for x in sym.primerange(0,100)]
         if number in Primelist:
             self.cash = self.cash*4
+        return self.cash
 
 
 
     def rangeof10(self):
         rangeof10_int = int(input('rangeend'))
         if (rangeof10_int < 10 or rangeof10_int > 100):
-                print('retry')
+            print('retry')
         else:
             rangeof10List = [x for x in range(rangeof10_int-10, rangeof10_int)]
             if number in rangeof10List:
                 self.cash = self.cash*10
+        return self.cash
         
     def oneToFiveNumbers(self):
         numlist_input = (input("1-5 Numbers").split(','))
@@ -61,8 +61,10 @@ class bets():
                 5: 20}
         if number in numlist_input:
             self.cash = self.cash*(oddsDict[len(numlist_input)])
-        
+        return self.cash
+     
 
+number = bets.rollWheel()
 
-
-Player = bets(cash)
+Player = bets(15000)
+print(Player.evenodd(1))
